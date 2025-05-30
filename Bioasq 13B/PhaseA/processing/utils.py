32703c8ac7c920bee22733378d2a72d27bde2cda
Mervin Jesus Sosa Borrero <sosam@utb.edu.co>
2025-05-30 13:56:02 -0500

    Add SciDiscourse notebook and resources in CheckThat_Subtask4a_SciDiscourse folder

--- Bioasq 13B/PhaseA/processing/utils.py
+++
@@ -1,9 +0,0 @@
-import re
-
-# Extraer los IDs de las URLs
-def extract_id(url):
-    try:
-      return re.search(r'(\d+)/?$', url).group(1)  # Extrae el número al final
-    except :
-      return "0"
-
